2d6213ef695f5be19f5bac83445605f35f421c81
Eric Wu <404835780@qq.com>
2017-08-09 08:40:44 +0800

    upload the file for week 4 assignment

---
+++ RunAnalysis.R
@@ -0,0 +1,87 @@
+
+RunAnalysis <- function()
+{
+## load useful packages in
+library(dplyr)
+library(reshape2)
+
+## load all train dataset
+train_x <- read.table("./train/X_train.txt")
+train_y <- read.table("./train/y_train.txt")
+train_sub <- read.table("./train/subject_train.txt")
+
+## load all test dataset
+test_x <- read.table("./test/X_test.txt")
+test_sub <- read.table("./test/subject_test.txt")
+test_y <- read.table("./test/y_test.txt")
+
+## get the real names
+feature <- read.table("./features.txt") 
+ini_names<-as.character(feature[,2])
+valid_names<-make.names(names=ini_names,unique=T,allow=T)
+
+test_df<-cbind(test_sub,test_x,test_y)
+train_df <- cbind(train_sub,train_x,train_y)
+
+names(test_df) <- c("subject", valid_names, "activity")
+test_df <- mutate(test_df, dataset = "test")
+
+names(train_df) <- c("subject", valid_names, "activity")
+train_df <- mutate(train_df, dataset = "train")
+
+## combine the two data set together train_df (7352x563) test_df (2947x563)
+df <- rbind(train_df,test_df)
+
+##label activities
+input<-df$activity
+setActivity <- function(input)
+{
+	index_1 <- which(input==1)
+	index_2 <- which(input==2)
+	index_3 <- which(input==3)
+	index_4 <- which(input==4)
+	index_5 <- which(input==5)
+	index_6 <- which(input==6)
+	input[index_1] <- "WALKING"
+	input[index_2] <- "WALKING_UPSTAIRS
+"
+	input[index_3] <- "WALKING_DOWNSTAIRS"
+	input[index_4] <- "SITTING"
+	input[index_5] <- "STANDING
+6"
+	input[index_6] <- "LAYING
+"
+
+	return(input)
+}
+df$activity <- setActivity(input)
+rm(input)
+
+## extract mean and std value for each variable
+data_mean<-sapply(df[,c(2:562)],mean, na.rm=TRUE)
+data_std<-sapply(df[,c(2:562)],sd, na.rm=TRUE)
+
+df_group<-group_by(df,subject,activity)
+
+## compute the average value for each group, take use of the valid_names variable
+## take use of the "summarise_if()" function, very handy when we need to summarise columns given some conditions
+
+## create a logical vetor
+create_tidy <- function(df)
+{
+	temp_names <- names(df)
+	temp_names <- temp_names[c(-1,-563)]
+	com_names <- temp_names
+	com_names[length(com_names)]<-"lastcol"
+	logic<-com_names==temp_names	
+}
+
+tidy_df<- summarise_if(df_group,create_tidy(df_group),mean)
+
+## alternatively, we can use "summarise_at()" to do this in a more convenient eay
+## tidy_df<-summarise_at(df_group,1:562,mean)
+
+## demonstrate and write the result into a txt file called "result.txt" in the current working directory
+View(tidy_df)
+write.table(tidy_df, "./result.txt",row.name = FALSE)
+}
