7d4b02b92c44743c0f01358a2dfd7542f718b3f0
Eric Wu <404835780@qq.com>
2017-08-10 08:48:15 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 
-runa_analysis <- function()
+run_analysis <- function()
 {
 ## load useful packages in
 library(dplyr)
@@ -9,27 +9,37 @@ library(reshape2)
 train_x <- read.table("./train/X_train.txt")
 train_y <- read.table("./train/y_train.txt")
 train_sub <- read.table("./train/subject_train.txt")
+colnames(train_sub)<-"subject"
+colnames(train_y)<-"activity"
 
 ## load all test dataset
 test_x <- read.table("./test/X_test.txt")
 test_sub <- read.table("./test/subject_test.txt")
 test_y <- read.table("./test/y_test.txt")
+colnames(test_sub)<-"subject"
+colnames(test_y)<-"activity"
 
-## get the real names
+## get the names
 feature <- read.table("./features.txt") 
 ini_names<-as.character(feature[,2])
-valid_names<-make.names(names=ini_names,unique=T,allow=T)
+t1<-grep("mean[^a-zA-Z]",ini_names)
+t2<-grep("std[^a-zA-Z]",ini_names)
 
-test_df<-cbind(test_sub,test_x,test_y)
-train_df <- cbind(train_sub,train_x,train_y)
+train_x_mean<-select(train_x,t1)
+names(train_x_mean)<-ini_names[t1]
+train_x_std <- select(train_x,t2)
+names(train_x_std)<-ini_names[t2]
+train_df <- cbind(train_sub,train_x_mean,train_x_std,train_y)
+train_df$subject <- as.character(train_df$subject)
 
-names(test_df) <- c("subject", valid_names, "activity")
-test_df <- mutate(test_df, dataset = "test")
+test_x_mean<-select(test_x,t1)
+names(test_x_mean)<-ini_names[t1]
+test_x_std <- select(test_x,t2)
+names(test_x_std)<-ini_names[t2]
+test_df<-cbind(test_sub,test_x_mean,test_x_std, test_y)
+test_df$subject <- as.character(test_df$subject)
 
-names(train_df) <- c("subject", valid_names, "activity")
-train_df <- mutate(train_df, dataset = "train")
-
-## combine the two data set together train_df (7352x563) test_df (2947x563)
+## combine the two data set together (10299x66)
 df <- rbind(train_df,test_df)
 
 ##label activities
@@ -43,14 +53,11 @@ setActivity <- function(input)
 	index_5 <- which(input==5)
 	index_6 <- which(input==6)
 	input[index_1] <- "WALKING"
-	input[index_2] <- "WALKING_UPSTAIRS
-"
+	input[index_2] <- "WALKING_UPSTAIRS"
 	input[index_3] <- "WALKING_DOWNSTAIRS"
 	input[index_4] <- "SITTING"
-	input[index_5] <- "STANDING
-6"
-	input[index_6] <- "LAYING
-"
+	input[index_5] <- "STANDING"
+	input[index_6] <- "LAYING"
 
 	return(input)
 }
@@ -58,19 +65,14 @@ df$activity <- setActivity(input)
 rm(input)
 
 ## extract mean and std value for each variable
-data_mean<-sapply(df[,c(2:562)],mean, na.rm=TRUE)
-data_std<-sapply(df[,c(2:562)],sd, na.rm=TRUE)
-
 df_group<-group_by(df,subject,activity)
 
-## compute the average value for each group, take use of the valid_names variable
 ## take use of the "summarise_if()" function, very handy when we need to summarise columns given some conditions
-
 ## create a logical vetor
 create_tidy <- function(df)
 {
 	temp_names <- names(df)
-	temp_names <- temp_names[c(-1,-563)]
+	temp_names <- temp_names[c(-1,-68)]
 	com_names <- temp_names
 	com_names[length(com_names)]<-"lastcol"
 	logic<-com_names==temp_names	
@@ -78,8 +80,7 @@ create_tidy <- function(df)
 
 tidy_df<- summarise_if(df_group,create_tidy(df_group),mean)
 
-## alternatively, we can use "summarise_at()" to do this in a more convenient eay
-## tidy_df<-summarise_at(df_group,1:562,mean)
+## alternatively, we can use "summarise_at()" to do this in a more convenient way
 
 ## demonstrate and write the result into a txt file called "result.txt" in the current working directory
 View(tidy_df)
